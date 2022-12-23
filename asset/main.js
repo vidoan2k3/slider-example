4ede9209aa13076c3aa5c8cc76b8d46f9963c81b
vidoan2k3 <89054611+vidoan2k3@users.noreply.github.com>
2022-12-23 13:09:10 +0700

    Add files via upload

---
+++ asset/main.js
@@ -0,0 +1,55 @@
+var dotBtn = document.querySelectorAll (".dot");
+var sliderImgs = document.querySelectorAll (".slider-container img")
+var nextBtn = document.querySelector (".next-btn")
+var preBtn = document.querySelector (".pre-btn")
+
+var currentIndex = 0
+var imgsLength = sliderImgs.length;
+var showImg = function (i) {
+    sliderImgs[i].classList.add ("active");
+    dotBtn[i].classList.add ("active");
+    sliderImgs.forEach ((value, index) => {
+        if (index != i) {
+            value.classList.remove ("active");
+        }
+    })
+    dotBtn.forEach ((value, index) => {
+        if (index != i) {
+            value.classList.remove ("active");
+        }
+    })
+}
+
+
+
+showImg (0);
+
+var dotEvent = function () {
+    dotBtn.forEach ((value, index) => {
+        value.addEventListener ('click', () => {
+            showImg (index);
+            currentIndex = index;
+        })
+    })
+}
+
+var nextImg = function () {
+        currentIndex++;
+        if (currentIndex > imgsLength-1) currentIndex = 0;
+        showImg (currentIndex);
+}
+
+
+var preImg = function () {
+        currentIndex--;
+        if (currentIndex < 0) currentIndex = imgsLength-1;
+        showImg (currentIndex);
+}
+
+var autoChange = function () {
+    window.setInterval (nextImg, 6000);
+}
+autoChange ();
+dotEvent();
+nextBtn.onclick = nextImg;
+preBtn.onclick = preImg;
